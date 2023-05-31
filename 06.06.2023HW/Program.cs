b24c911b9a9a170f1ded0a1cfb0a00b99755024f
dweavp <100860878+pashatett@users.noreply.github.com>
2023-05-31 21:47:11 +0300

    Update Program.cs

--- 06.06.2023HW/Program.cs
+++ 06.06.2023HW/Program.cs
@@ -6,9 +6,110 @@ using System.Threading.Tasks;
 
 namespace _6._06._2023HW
 {
+    class Task4
+    {
+        string nameSite;
+        string pathSite;
+        string descript;
+        int ipSite;
+
+        public void SetName() { nameSite = Console.ReadLine(); }
+        public string GetName() { return nameSite; }
+        public void SetPath() { pathSite = Console.ReadLine(); }
+        public string GetPath() { return pathSite; }
+        public void SetDesc() { descript = Console.ReadLine(); }
+        public string GetDesc() { return descript; }
+        public void SetIP() { ipSite = int.Parse(Console.ReadLine()); }
+        public int GetIP() { return ipSite;}
+        public void SetAll()
+        {
+            nameSite = Console.ReadLine();
+            pathSite = Console.ReadLine();
+            descript = Console.ReadLine();
+            ipSite = int.Parse(Console.ReadLine());
+        }
+        public void ShowAll()
+        {
+            Console.WriteLine(nameSite);
+            Console.WriteLine(pathSite);
+            Console.WriteLine(descript);
+            Console.WriteLine(ipSite);
+        }
+    }
+    class Task5
+    {
+        string nameMagazine;
+        int yearOsn;
+        string opisanie;
+        int number;
+        string email;
+        
+
+        public void SetName() { nameMagazine = Console.ReadLine(); }
+        public string GetName() { return nameMagazine; }
+        public void SetOpis() { opisanie = Console.ReadLine(); }
+        public string GetOpis() { return opisanie; }
+        public void SetEmail() { email = Console.ReadLine(); }
+        public string GetEmail() { return email; }
+        public void SetYear() { yearOsn = int.Parse(Console.ReadLine()); }
+        public int GetYear() { return yearOsn; }
+        public void SetNumber() { number = int.Parse(Console.ReadLine()); }
+        public int GetNumber() { return number; }
+        public void SetAll()
+        {
+            nameMagazine = Console.ReadLine();
+            opisanie = Console.ReadLine();
+            email = Console.ReadLine();
+            yearOsn = int.Parse(Console.ReadLine());
+            number = int.Parse(Console.ReadLine());
+        }
+        public void ShowAll()
+        {
+            Console.WriteLine(nameMagazine);
+            Console.WriteLine(opisanie);
+            Console.WriteLine(email);
+            Console.WriteLine(yearOsn);
+            Console.WriteLine(number);
+        }
+    }
+    class Task6
+    {
+        string nameShop;
+        string adres;
+        string opisanie;
+        int number;
+        string email;
+
+
+        public void SetName() { nameShop = Console.ReadLine(); }
+        public string GetName() { return nameShop; }
+        public void SetOpis() { opisanie = Console.ReadLine(); }
+        public string GetOpis() { return opisanie; }
+        public void SetEmail() { email = Console.ReadLine(); }
+        public string GetEmail() { return email; }
+        public void SetAdres() { adres = Console.ReadLine(); }
+        public string GetAdres() { return adres; }
+        public void SetNumber() { number = int.Parse(Console.ReadLine()); }
+        public int GetNumber() { return number; }
+        public void SetAll()
+        {
+            nameShop = Console.ReadLine();
+            opisanie = Console.ReadLine();
+            email = Console.ReadLine();
+            adres = Console.ReadLine();
+            number = int.Parse(Console.ReadLine());
+        }
+        public void ShowAll()
+        {
+            Console.WriteLine(nameShop);
+            Console.WriteLine(opisanie);
+            Console.WriteLine(email);
+            Console.WriteLine(adres);
+            Console.WriteLine(number);
+        }
+    }
     internal class Program
     {
-        //012345
         static void task1(int Len, char sym)
         {
             char[,] arr = new char[Len, Len];
